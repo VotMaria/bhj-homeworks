cf759e6c0001b002987f8fc0225bcd96d1a9f82d
VotMaria <votmariya@yandex.ru>
2023-07-18 20:15:50 +0300

    Счетчик_сделано

---
+++ js-features/countdown/task.js
@@ -0,0 +1,16 @@
+               
+               
+               let countDown = function() {
+                    let timer = document.getElementById("timer");
+                    timer.textContent -= 1;  
+                                     
+               
+
+                if(timer.textContent < 0) {
+                    alert("Вы победили в конкурсе!");
+                    clearInterval(timerId);
+                }  
+
+                
+            }
+            let timerId = setInterval(countDown, 1000); 
