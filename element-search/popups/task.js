0bc978ba85622977ac7d2a30e5554f53eb5fc788
VotMaria <votmariya@yandex.ru>
2023-08-03 13:19:06 +0300

    Попапс_сделано

---
+++ element-search/popups/task.js
@@ -0,0 +1,20 @@
+let windows = document.getElementById('modal_main')
+
+windows.classList.add('modal_active')
+
+let windowsClose = document.querySelectorAll('.modal__close')
+
+for (let element of windowsClose) {
+    element.addEventListener('click', () => {
+      element.closest('.modal').classList.remove('modal_active')
+      })
+    }
+
+    let btn = document.querySelector('.btn')
+    let modalSuccess = document.getElementById('modal_success')
+
+     btn.addEventListener('click', () => {
+      modalSuccess.classList.add('modal_active')
+    })
+
+   
