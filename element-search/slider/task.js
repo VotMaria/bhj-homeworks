57a7332aba7ab7fb77ab5d22e54e0980b83966c8
VotMaria <votmariya@yandex.ru>
2023-08-03 13:17:17 +0300

    Слайды_сделано

---
+++ element-search/slider/task.js
@@ -0,0 +1,21 @@
+const sliders = Array.from(document.getElementsByClassName('slider__item'));
+
+const arrowPrev = document.getElementsByClassName('slider__arrow_prev')[0];
+
+const arrowNext = document.getElementsByClassName('slider__arrow_next')[0];
+
+
+
+arrowPrev.onclick = () => {
+const activeSlider = sliders.findIndex (currentSlide => currentSlide.classList.contains('slider__item_active'));
+sliders[activeSlider].classList.remove('slider__item_active');
+const nextSlider = activeSlider > 0 ? activeSlider - 1 : sliders.length - 1;
+sliders[nextSlider].classList.add('slider__item_active');
+}
+
+arrowNext.onclick = () => {
+    const activeSlider = sliders.findIndex (currentSlide => currentSlide.classList.contains('slider__item_active'));
+    sliders[activeSlider].classList.remove('slider__item_active');
+    const nextSlider = activeSlider < sliders.length - 1 ? activeSlider + 1 : 0;
+    sliders[nextSlider].classList.add('slider__item_active');   
+}
