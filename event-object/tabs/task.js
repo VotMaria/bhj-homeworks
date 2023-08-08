96e442a23011ba12c86413723b2fc9d44187485b
VotMaria <votmariya@yandex.ru>
2023-08-08 19:59:36 +0300

    Вкладки_сделано

---
+++ event-object/tabs/task.js
@@ -0,0 +1,22 @@
+const tab = Array.from(document.querySelectorAll('.tab')) 
+const content = Array.from(document.querySelectorAll('.tab__content')) 
+
+
+tab.forEach((element, index) => {
+  element.addEventListener ('click', (e) => {
+
+    for(let element of tab) {
+      element.classList.remove('tab_active')
+  }
+
+    for (let element of content) {
+    element.classList.remove('tab__content_active')  
+    }
+   
+    tab[index].classList.add('tab_active')
+    content[index].classList.add('tab__content_active')
+
+  e.preventDefault();
+  })
+
+  })  
