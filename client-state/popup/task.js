1e8a45d78c7e39d485fc319bceb33ce3a310f78c
VotMaria <votmariya@yandex.ru>
2023-08-14 15:31:46 +0300

    popup_ready

---
+++ client-state/popup/task.js
@@ -0,0 +1,28 @@
+let windows = document.getElementById('subscribe-modal')
+windows.classList.add('modal_active')
+let windowsClose = document.querySelectorAll('.modal__close')
+
+window.onload = () => {
+  if(!'Cookie') {
+    element.closest('.modal').classList.remove('modal_active');
+  }
+}
+
+for (let element of windowsClose) {
+    element.addEventListener('click', (e) => {
+        element.closest('.modal').classList.remove('modal_active');  
+        document.cookie = 'Cookie=CookieClose';   
+        e.preventDefault();  
+      });
+    }
+
+  
+
+      
+    
+    
+
+    
+   
+
+   
