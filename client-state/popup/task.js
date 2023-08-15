cb1ef32f86811e673d6a7370d2a7572b6a16f5a2
VotMaria <votmariya@yandex.ru>
2023-08-15 19:38:24 +0300

    popup_change4

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -2,8 +2,9 @@ let windows = document.getElementById('subscribe-modal')
 windows.classList.add('modal_active')
 let windowsClose = document.querySelectorAll('.modal__close')
 
+
 window.onload = () => {
-  if(!document.cookie.name) {
+  if(!document.cookie.includes('name=CookieClose')) {     
     for (let element of windowsClose) {
       element.addEventListener('click', (e) => {
           element.closest('.modal').classList.remove('modal_active');
@@ -14,12 +15,11 @@ window.onload = () => {
 
 for (let element of windowsClose) {
     element.addEventListener('click', (e) => {
-        element.closest('.modal').classList.remove('modal_active');  
-        document.cookie = 'name=CookieClose';  
+       element.closest('.modal').classList.remove('modal_active');
+        document.cookie = 'name=CookieClose'; 
         e.preventDefault();  
       });
 }   
-
 
 
 
