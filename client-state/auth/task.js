3aa40d23add29cbabd76fd70461f50aa82a20b46
VotMaria <votmariya@yandex.ru>
2023-08-15 16:31:50 +0300

    auth_change_1

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,9 +1,50 @@
+
 let signinForm = document.getElementById('signin__form')
 let welcome = document.getElementById('welcome')
 let userId = document.getElementById('user_id')
 let form = document.getElementById('signin')
 
+window.onload = () => {
+  if(localStorage.id) {
+    form.classList.remove('signin_active');
+    welcome.classList.add('welcome_active');
+    welcome.textContent += localStorage.id;
+    console.log(localStorage.id)
+  } 
+}  
+
 signinForm.addEventListener('submit', (e) => {
+
+  let formData = new FormData(signinForm);
+  let request = new XMLHttpRequest();
+  request.responseType = 'json';
+  request.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth'); 
+    
+  request.onload = () => {
+    let answerFromServer = request.response;
+    let id = answerFromServer.user_id;
+    localStorage.setItem('id', id);
+      
+    if(id) {
+      form.classList.remove('signin_active');
+      welcome.classList.add('welcome_active');
+      welcome.textContent += id
+    } else {
+      alert('Неверный логин/пароль');
+      }
+  }
+      request.send(formData);
+      e.preventDefault();
+})
+    
+
+
+
+
+
+
+
+/*signinForm.addEventListener('submit', (e) => {
 
   let formData = new FormData(signinForm);
   let request = new XMLHttpRequest();
@@ -25,7 +66,7 @@ signinForm.addEventListener('submit', (e) => {
     }
     request.send(formData);
     e.preventDefault();
-})
+})*/
 
    
 
