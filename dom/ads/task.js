9192868f5907997897243341d65f85728c6666d9
VotMaria <votmariya@yandex.ru>
2023-09-19 18:08:37 +0300

    ads_ready

---
+++ dom/ads/task.js
@@ -0,0 +1,27 @@
+let arrText = Array.from(document.querySelectorAll('.rotator__case'));
+console.log(arrText);
+
+let index = 0;
+
+function infinity() {
+    arrText.forEach((element) => {
+      element.classList.remove('rotator__case_active');
+    })
+
+    //index = (index + 1) % arrText.length;
+    
+    index += 1;
+    if (index === arrText.length) {
+        index = 0;
+    }
+
+    let nextStep = arrText[index];
+    nextStep.classList.add('rotator__case_active')
+}
+      
+setInterval (() => {
+  infinity();
+}, 1000)
+
+
+
