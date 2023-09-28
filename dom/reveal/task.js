e7815d022cbb22cecc22edf65b398ac13f1701c0
VotMaria <votmariya@yandex.ru>
2023-09-28 13:43:23 +0300

    reveal_ready

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -2,11 +2,13 @@ let reveal = Array.from(document.querySelectorAll('.reveal'));
 console.log(reveal);
 
 window.addEventListener('scroll', () => {
-for (let element of reveal){
-    const { top, bottom } = element.getBoundingClientRect();
-    if(bottom < 0 || top > window.innerHeight){
-        element.classList.add('reveal_active')
-    }
-} 
+	for (let element of reveal) {
+		const {
+			top,
+			bottom
+		} = element.getBoundingClientRect();
+		if (bottom < 0 || top > window.innerHeight) {
+			element.classList.add('reveal_active')
+		}
+	}
 })
-
