bec40bd98460d7c9365358357df13df21bf47ba0
VotMaria <votmariya@yandex.ru>
2023-09-28 13:56:00 +0300

    preloader_ready

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -6,35 +6,35 @@ let animation = document.getElementById('loader')
 
 xhr.addEventListener('readystatechange', (e) => {
 
-    if(xhr.readyState === xhr.DONE) {
-      animation.classList.remove('loader_active')
-      console.log(xhr.responseText)
+	if (xhr.readyState === xhr.DONE) {
+		animation.classList.remove('loader_active')
+		console.log(xhr.responseText)
 
-      let answerFromServer = JSON.parse(xhr.responseText);
+		let answerFromServer = JSON.parse(xhr.responseText);
 
-      let outputCharCode;
-      let outputValue;
-      let outputCurrency = '<div class="item__currency">' + 'руб.' + '</div>';
-      let output = '';
+		let outputCharCode;
+		let outputValue;
+		let outputCurrency = '<div class="item__currency">' + 'руб.' + '</div>';
+		let output = '';
 
-      for(let key in answerFromServer) {
-        let objValute = answerFromServer[key].Valute;
-        console.log(objValute)
+		for (let key in answerFromServer) {
+			let objValute = answerFromServer[key].Valute;
+			console.log(objValute)
 
-        for(let key in objValute) {
-          outputCharCode = '<div class="item__code">' + objValute[key].CharCode + '</div>';
-          console.log(outputCharCode)
+			for (let key in objValute) {
+				outputCharCode = '<div class="item__code">' + objValute[key].CharCode + '</div>';
+				console.log(outputCharCode)
 
-          outputValue = '<div class="item__value">' + objValute[key].Value + '</div>';
-          console.log(outputValue)
+				outputValue = '<div class="item__value">' + objValute[key].Value + '</div>';
+				console.log(outputValue)
 
-          output += '<div class="item">' + outputCharCode + outputValue + outputCurrency + '</div>';
-      }
-    }
+				output += '<div class="item">' + outputCharCode + outputValue + outputCurrency + '</div>';
+			}
+		}
 
-      document.getElementById('items').innerHTML = output;
+		document.getElementById('items').innerHTML = output;
 
 
-    }
-  e.preventDefault();
+	}
+	e.preventDefault();
 })
