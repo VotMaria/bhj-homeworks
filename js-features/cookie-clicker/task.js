1bccfd2bb34a27a26e0e58805f764e60a62e33ab
VotMaria <votmariya@yandex.ru>
2023-09-28 13:32:58 +0300

    cookie-clicker_ready

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -1,12 +1,12 @@
-    let clickerCounter = document.getElementById('clicker__counter');
-    let image = document.getElementById('cookie');
+let clickerCounter = document.getElementById('clicker__counter');
+let image = document.getElementById('cookie');
 
-    image.onclick = function() {
-        clickerCounter.textContent ++;
+image.onclick = function() {
+    clickerCounter.textContent++;
 
-        if(clickerCounter.textContent % 2 === 0) {
-            image.width = 500;
-        } else {
-            image.width = 200;
-        }
+    if (clickerCounter.textContent % 2 === 0) {
+        image.width = 500;
+    } else {
+        image.width = 200;
     }
+}
