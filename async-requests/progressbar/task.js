b4be39e9aa563ca34cb1ee9ad0ed5fa3f6128554
VotMaria <votmariya@yandex.ru>
2023-09-28 13:57:05 +0300

    progressbar_ready

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -3,16 +3,16 @@ let progress = document.getElementById('progress')
 
 message.addEventListener('submit', (e) => {
 
-  let formData = new FormData(message);
-  let request = new XMLHttpRequest();
-  request.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
-  
-  
-  request.upload.onprogress = (e) => {
-    let progressLoaded = e.loaded/e.total;
-    progress.value = progressLoaded; 
-    }
+	let formData = new FormData(message);
+	let request = new XMLHttpRequest();
+	request.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload');
 
-  request.send(formData);
-  e.preventDefault();
+
+	request.upload.onprogress = (e) => {
+		let progressLoaded = e.loaded / e.total;
+		progress.value = progressLoaded;
+	}
+
+	request.send(formData);
+	e.preventDefault();
 })
