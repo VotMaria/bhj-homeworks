004258b3e35f359e0fba0a7719d2bb812ff134b0
VotMaria <votmariya@yandex.ru>
2023-09-28 13:35:58 +0300

    menu_ready

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,10 +1,10 @@
 let menuLink = document.querySelectorAll('.menu__link');
-for(let element of menuLink){
-    element.addEventListener('click', (e) => {
-        let menuSub = element.closest('.menu__item').querySelector('.menu_sub');        
-        if(menuSub) {
-            e.preventDefault();
-            menuSub.classList.toggle('menu_active');
-        }
-    })
+for (let element of menuLink) {
+	element.addEventListener('click', (e) => {
+		let menuSub = element.closest('.menu__item').querySelector('.menu_sub');
+		if (menuSub) {
+			e.preventDefault();
+			menuSub.classList.toggle('menu_active');
+		}
+	})
 }
