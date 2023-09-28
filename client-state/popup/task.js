3a2b7a96e4ed7cfa879b3b4ab726ca91ad1d0a92
VotMaria <votmariya@yandex.ru>
2023-09-28 13:58:48 +0300

    popup_ready

--- client-state/popup/task.js
+++ client-state/popup/task.js
@@ -1,35 +1,11 @@
-/*let windows = document.getElementById('subscribe-modal')
-let windowsClose = document.querySelectorAll('.modal__close')
-
-
-window.onload = () => {
-  if(document.cookie.includes('name=CookieClose')) {     
-    for (let element of windowsClose) {
-      element.addEventListener('click', (e) => {
-          element.closest('.modal').classList.remove('modal_active');
-      })
-    }
-  }
-}
-
-for (let element of windowsClose) {
-    element.addEventListener('click', (e) => {
-       element.closest('.modal').classList.remove('modal_active');
-        document.cookie = 'name=CookieClose'; 
-        e.preventDefault();  
-      });
-}  */ 
-
 const windows = document.querySelector('#subscribe-modal');
 const windowsClose = document.querySelector('.modal__close');
 
-if(!document.cookie.includes('name=CookieClose')) {
-  windows.classList.add('modal_active');
+if (!document.cookie.includes('name=CookieClose')) {
+	windows.classList.add('modal_active');
 }
 
 windowsClose.addEventListener('click', () => {
-  windows.classList.remove('modal_active');
-  document.cookie = 'name=CookieClose';
+	windows.classList.remove('modal_active');
+	document.cookie = 'name=CookieClose';
 })
-
-
