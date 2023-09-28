a6df8809d70a2982fcf5c9e4be46cd79ca2a185c
VotMaria <votmariya@yandex.ru>
2023-09-28 13:37:55 +0300

    slider_ready

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -7,15 +7,15 @@ const arrowNext = document.getElementsByClassName('slider__arrow_next')[0];
 
 
 arrowPrev.onclick = () => {
-const activeSlider = sliders.findIndex (currentSlide => currentSlide.classList.contains('slider__item_active'));
-sliders[activeSlider].classList.remove('slider__item_active');
-const nextSlider = activeSlider > 0 ? activeSlider - 1 : sliders.length - 1;
-sliders[nextSlider].classList.add('slider__item_active');
+	const activeSlider = sliders.findIndex(currentSlide => currentSlide.classList.contains('slider__item_active'));
+	sliders[activeSlider].classList.remove('slider__item_active');
+	const nextSlider = activeSlider > 0 ? activeSlider - 1 : sliders.length - 1;
+	sliders[nextSlider].classList.add('slider__item_active');
 }
 
 arrowNext.onclick = () => {
-    const activeSlider = sliders.findIndex (currentSlide => currentSlide.classList.contains('slider__item_active'));
-    sliders[activeSlider].classList.remove('slider__item_active');
-    const nextSlider = activeSlider < sliders.length - 1 ? activeSlider + 1 : 0;
-    sliders[nextSlider].classList.add('slider__item_active');   
+	const activeSlider = sliders.findIndex(currentSlide => currentSlide.classList.contains('slider__item_active'));
+	sliders[activeSlider].classList.remove('slider__item_active');
+	const nextSlider = activeSlider < sliders.length - 1 ? activeSlider + 1 : 0;
+	sliders[nextSlider].classList.add('slider__item_active');
 }
