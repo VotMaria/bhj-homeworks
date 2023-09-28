4b2f676e3c18d2491946444b6efba4e5c9dfcca1
VotMaria <votmariya@yandex.ru>
2023-09-28 13:33:58 +0300

    countdown_ready

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,16 +1,14 @@
-               
-               
-               let countDown = function() {
-                    let timer = document.getElementById("timer");
-                    timer.textContent -= 1;  
-                                     
-               
+let countDown = function() {
+    let timer = document.getElementById("timer");
+    timer.textContent -= 1;
 
-                if(timer.textContent < 0) {
-                    alert("Вы победили в конкурсе!");
-                    clearInterval(timerId);
-                }  
 
-                
-            }
-            let timerId = setInterval(countDown, 1000); 
+
+    if (timer.textContent < 0) {
+        alert("Вы победили в конкурсе!");
+        clearInterval(timerId);
+    }
+
+
+}
+let timerId = setInterval(countDown, 1000);
