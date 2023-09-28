d94c4c169f66504e92850407dd41b8936d2b01e4
VotMaria <votmariya@yandex.ru>
2023-09-28 13:55:03 +0300

    poll_ready

--- async-requests/poll/task.js
+++ async-requests/poll/task.js
@@ -1,39 +1,35 @@
 const btn = document.getElementsByTagName('button')
-console.log(btn)
 
 let xhr = new XMLHttpRequest();
 xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll')
 xhr.send();
 
 xhr.addEventListener('readystatechange', (e) => {
-    if(xhr.readyState === xhr.DONE) {
-        console.log(xhr.responseText)
+	if (xhr.readyState === xhr.DONE) {
+		console.log(xhr.responseText)
 
-        let answerFromServer = JSON.parse(xhr.responseText);
-        let output;
-        let answer = '';
+		let answerFromServer = JSON.parse(xhr.responseText);
+		let output;
+		let answer = '';
 
-        for(let key in answerFromServer) {
-          output = answerFromServer[key].title;
-        
-          let arrAnswer = answerFromServer[key].answers;
+		for (let key in answerFromServer) {
+			output = answerFromServer[key].title;
 
-          for(let key in arrAnswer) {
-           answer += '<button>' + arrAnswer[key] + '</button> ';
-          }
-        }
+			let arrAnswer = answerFromServer[key].answers;
 
-        document.getElementById('poll__title').innerHTML = output;
-        document.getElementById('poll__answers').innerHTML = answer;
-    }   
-    e.preventDefault();   
+			for (let key in arrAnswer) {
+				answer += '<button>' + arrAnswer[key] + '</button> ';
+			}
+		}
 
-    for(let element of btn) {
-      element.addEventListener('click', () => {
-        alert('Спасибо, ваш голос засчитан!')
-      })
-    }
+		document.getElementById('poll__title').innerHTML = output;
+		document.getElementById('poll__answers').innerHTML = answer;
+	}
+	e.preventDefault();
+
+	for (let element of btn) {
+		element.addEventListener('click', () => {
+			alert('Спасибо, ваш голос засчитан!')
+		})
+	}
 })
-
-
-
